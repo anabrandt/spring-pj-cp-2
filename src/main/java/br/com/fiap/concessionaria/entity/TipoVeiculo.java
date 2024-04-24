4f31805f271d72d64fdef1e53d3597cfee31c576
Ana Carolina <anactavaresbrand@gmail.com>
2024-04-24 18:53:59 -0300

    TipoVeiculo

--- src/main/java/br/com/fiap/concessionaria/entity/TipoVeiculo.java
+++ src/main/java/br/com/fiap/concessionaria/entity/TipoVeiculo.java
@@ -2,13 +2,13 @@ package br.com.fiap.concessionaria.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @Builder
@@ -22,8 +22,11 @@ import lombok.NoArgsConstructor;
         @UniqueConstraint(name = "UK_TIPO_NOME", columnNames = {"NOME"})
 })
 public class TipoVeiculo {
+
+    @Id // Indica que essa propriedade é a chave primária da entidade
     @Column(name = "ID_TIPO_VEICULO")
     private Long id;
+
     @Column(name = "NOME")
     private String nome;
 }
